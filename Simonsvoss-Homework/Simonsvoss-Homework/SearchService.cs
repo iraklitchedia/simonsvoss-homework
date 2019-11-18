aa3b49ab29310cf6ad5a93211eff18c075b5dc31
Irakli Tchedia <20854592+iraklitchedia@users.noreply.github.com>
2019-11-18 12:00:36 +0100

    Added dictionary

--- Simonsvoss-Homework/Simonsvoss-Homework/SearchService.cs
+++ Simonsvoss-Homework/Simonsvoss-Homework/SearchService.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using Newtonsoft.Json;
 using Simonsvoss_Homework.Models;
 
@@ -8,13 +9,25 @@ namespace Simonsvoss_Homework
   {
     private readonly string _filepath = @".\Data\sv_lsm_data.json";
     private Data _data;
+    // Dictionary will help to access data using Ids
+    private readonly Dictionary<string, Entity> _dict;
 
     public SearchService()
     {
+      _dict = new Dictionary<string, Entity>();
+
       // We do it here to avoid reading file every time search is requested
       GetDataFromJsonFile();
+
+      // Setup dictionary
+      InitDictionary();
     }
 
+    /// <summary>
+    /// Search text between the entities
+    /// </summary>
+    /// <param name="text"></param>
+    /// <returns></returns>
     public string Search(string text)
     {
       return JsonConvert.SerializeObject(_data);
@@ -29,6 +42,44 @@ namespace Simonsvoss_Homework
       // Read file and deserialize from json
       var content = File.ReadAllText(_filepath);
       _data = JsonConvert.DeserializeObject<Data>(content);
+    }
+
+    /// <summary>
+    /// Add entities to dictionary
+    /// </summary>
+    private void InitDictionary()
+    {
+      foreach (var buildingItem in _data.buildings)
+      {
+        _dict.Add(buildingItem.Id, buildingItem);
+      }
+
+      foreach (var lockItem in _data.locks)
+      {
+        _dict.Add(lockItem.Id, lockItem);
+
+        // Match Locks to Buildings
+        if (!string.IsNullOrEmpty(lockItem.BuildingId) && _dict.ContainsKey(lockItem.BuildingId))
+        {
+          ((Building) _dict[lockItem.BuildingId]).Locks.Add(lockItem.Id);
+        }
+      }
+
+      foreach (var groupItem in _data.groups)
+      {
+        _dict.Add(groupItem.Id, groupItem);
+      }
+
+      foreach (var mediumItem in _data.media)
+      {
+        _dict.Add(mediumItem.Id, mediumItem);
+
+        // Match Mediums to Groups
+        if (!string.IsNullOrEmpty(mediumItem.GroupId) && _dict.ContainsKey(mediumItem.GroupId))
+        {
+          ((Group) _dict[mediumItem.GroupId]).Media.Add(mediumItem.Id);
+        }
+      }
     }
   }
 }
